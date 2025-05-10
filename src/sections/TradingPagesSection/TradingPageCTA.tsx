f8ab9d3fff3cd1607d19f5f21f5e15c0ce198f1a
AnslemCode <102703239+AnslemCode@users.noreply.github.com>
2025-05-10 11:37:40 +0100

    chore: name change

--- src/sections/TradingPagesSection/TradingPageCTA.tsx
+++ src/sections/TradingPagesSection/TradingPageCTA.tsx
@@ -43,7 +43,7 @@ const TradingPageCTA: React.FC<TradingPageCTAProps> = ({
     : "Trade confidently and securely with AssexMarkets today.";
 
   const imageSrc = isDemoPage
-    ? "/images/demoPageImage.svg"
+    ? "/images/simulatedPageImage.svg"
     : "/images/cta-graphic.png";
 
   const imageWidth = isDemoPage ? 708 : 520;
